34c104cbb7082577b3ee95dc2cfb1631ac328ca5
andreaw1994 <55756706+andreaw1994@users.noreply.github.com>
2024-09-07 15:55:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,12 @@ st.write(
     "This app allows you to interactively visualize and analyze key performance metrics and error patterns of a STE1 production run."
 )
 
-data = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/Hackathon_Bayer/data/MasterCSV/ppq1.csv")
+import pandas as pd
+
+# Use Streamlit's file uploader
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    # Read the uploaded CSV file
+    data = pd.read_csv(uploaded_file)
+    st.write(data)
