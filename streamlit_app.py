3d3ee327a1eeed15763c78134e2aa508e584040f
andreaw1994 <55756706+andreaw1994@users.noreply.github.com>
2024-09-07 15:48:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ st.title("STE1 Manufacturing Analyzer")
 st.write(
     "This app allows you to interactively visualize and analyze key performance metrics and error patterns of a STE1 production run."
 )
+
+data = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/Hackathon_Bayer/data/MasterCSV/ppq1.csv")
