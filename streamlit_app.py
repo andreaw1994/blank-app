6a80cd25495880c88e795550a331fc630c4cd592
andreaw1994 <55756706+andreaw1994@users.noreply.github.com>
2024-09-07 15:41:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ from IPython.display import display
 
 st.title("STE1 Manufacturing Analyzer")
 st.write(
-    "This app allows you to interactively visualize and analyze key performance and error metrics of a STE1 production run."
+    "This app allows you to interactively visualize and analyze key performance metrics and error patterns of a STE1 production run."
 )
