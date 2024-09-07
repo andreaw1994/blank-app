fe5e9566455c644986c1718d81e18062619d9fcb
andreaw1994 <55756706+andreaw1994@users.noreply.github.com>
2024-09-07 16:16:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-#import pandas as pd
-#import matplotlib.pyplot as plt
+
 #import ipywidgets as widgets
 #from IPython.display import display
 
@@ -32,3 +31,30 @@ data["derivative_good_objects"] = data[data["good_objects"].notna()]["good_objec
 data["derivative_good_objects"] = data["derivative_good_objects"].fillna(0)
 
 data
+
+data["zero_time"] = datetime.timedelta()
+prev_der = None
+first_time = data.loc[data["good_objects"].first_valid_index(), "datetime"]
+start_from = data.loc[data["good_objects"].first_valid_index(), "good_objects"]
+
+for i, row in enumerate(data.itertuples()):
+    # Transition from 0 to positive derivative
+    if prev_der is not None and row.derivative_good_objects > 0 and prev_der == 0 and row.good_objects != start_from:
+        zero_time = (row.datetime - first_time)
+        data.at[i, "zero_time"] = zero_time
+
+    # Transition from positive to 0 derivative
+    elif prev_der is not None and row.derivative_good_objects == 0 and prev_der > 0:
+        first_time = row.datetime  # Start the zero time interval
+
+    # Update the previous derivative for the next iteration
+    prev_der = row.derivative_good_objects
+
+sorted_times = data[data["zero_time"] > datetime.timedelta()]["zero_time"].sort_values(ascending = False)
+
+fig, ax1 = plt.subplots()
+fig.set_size_inches(28.5, 15.5)
+
+import matplotlib.pyplot as plt
+
+ax1.scatter(data[data["zero_time"] > datetime.timedelta()]["datetime"], data[data["zero_time"] > datetime.timedelta()]["zero_time"].dt.total_seconds())
