81e106dbbead48f7316e7e497d07442486b18108
andreaw1994 <55756706+andreaw1994@users.noreply.github.com>
2024-09-07 15:07:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import ipywidgets as widgets
+from IPython.display import display
 
-st.title("🎈 My new app")
+st.title("STE1 Manufacturing Analyzer")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
